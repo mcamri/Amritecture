c7610a29b57581d79617517a4558d52d94cc70fb
amri.mc <amri.mc.ios@gmail.com>
2022-01-14 21:54:20 +0800

    Initial Commit

---
+++ Amritecture/AmritectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmritectureApp.swift
+//  Amritecture
+//
+//  Created by amri.mc on 14/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AmritectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Amritecture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Amritecture
+//
+//  Created by amri.mc on 14/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
